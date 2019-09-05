60e31ed57350998e0bf299e21050d51bdbc6f193
Rostyslav Lytvyn <lytvynrostyk@gmail.com>
2019-09-05 23:25:16 +0300

    Add bottom navigator

--- src/navigators/MainNavigator.js
+++ src/navigators/MainNavigator.js
@@ -1,11 +1,22 @@
-import {createStackNavigator} from 'react-navigation-stack';
+import React from 'react';
 import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import {Icon, Text} from 'react-native-elements';
 
-const AppNavigator = createStackNavigator(
+const AppNavigator = createBottomTabNavigator(
   {
     Home: {
       screen: HomeScreen,
+      navigationOptions: {
+        title: 'Search',
+        tabBarIcon: ({focused, horizontal, tintColor}) => (
+          <Icon color={'#F44336'} name={'search'} type="material" />
+        ),
+        tabBarLabel: ({focused, tintColor}) => (
+          <Text style={{color: '#FF9800', fontSize: 14}}>Search</Text>
+        )
+      },
     },
   },
   {
