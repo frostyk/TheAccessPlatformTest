a1b663a51850a29d2889d92614676640551729d6
Rostyslav Lytvyn <lytvynrostyk@gmail.com>
2019-09-05 14:59:36 +0300

    Add action creators tests

--- __tests__/srs/components/Autocomplete-test.js
+++ __tests__/srs/components/Autocomplete-test.js
@@ -1,22 +1,23 @@
 import 'react-native';
 import React from 'react';
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import Autocomplete from '../../../src/components/Autocomplete';
 import {getVisiblePlaces} from '../../../src/redux/selectors/places';
 import places from '../../../src/__fixtures__/places';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Autocomplete
-      data={getVisiblePlaces(places, '')}
-      updateSearch={jest.fn()}
-      value={'search'}
-      onItemClick={jest.fn()}
-      itemIcon={'room'}
-      placeholder={'Type Country or City...'}
-    />,
-  );
+  const tree = renderer
+    .create(
+      <Autocomplete
+        data={getVisiblePlaces(places, '')}
+        updateSearch={jest.fn()}
+        value={'search'}
+        onItemClick={jest.fn()}
+        itemIcon={'room'}
+        placeholder={'Type Country or City...'}
+      />,
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
