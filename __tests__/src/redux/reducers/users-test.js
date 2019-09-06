4b97f8d31d9b6ec284b28235b07fe47e0963779e
Rostyslav Lytvyn <lytvynrostyk@gmail.com>
2019-09-06 11:10:43 +0300

    Fix tests

--- __tests__/src/redux/reducers/users-test.js
+++ __tests__/src/redux/reducers/users-test.js
@@ -4,10 +4,10 @@ import users from '../../../../src/__fixtures__/users';
 
 it('should setup default users values', () => {
   const state = usersReducer(undefined, {type: '@@INIT'});
-  expect(state).toEqual([]);
+  expect(state).toEqual({selectedUser: {}, users: []});
 });
 
 it('should set users', () => {
   const state = usersReducer(undefined, {type: SET_USERS, users});
-  expect(state).toEqual(users);
+  expect(state).toEqual({selectedUser: {}, users});
 });
